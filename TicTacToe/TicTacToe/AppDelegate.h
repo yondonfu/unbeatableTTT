1250ea9e89eaaddd142bae0ac7a4af13870a9df5
Yondon Fu <y.fu710@gmail.com>
2015-05-20 13:55:54 -0400

    set up xcode project

---
+++ TicTacToe/TicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Yondon Fu on 5/20/15.
+//  Copyright (c) 2015 TicTacToe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicTacToe/TicTacToe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Yondon Fu on 5/20/15.
+//  Copyright (c) 2015 TicTacToe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
