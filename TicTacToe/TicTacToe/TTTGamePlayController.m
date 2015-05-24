8876b38382455687ee7132d2d76d9fe72be639c8
Yondon Fu <y.fu710@gmail.com>
2015-05-24 12:33:57 -0400

    some tweaks to game view, added checks for winner

--- TicTacToe/TicTacToe/TTTGamePlayController.m
+++ TicTacToe/TicTacToe/TTTGamePlayController.m
@@ -29,4 +29,139 @@
     return YES;
 }
 
+- (BOOL)hasWonPlayer:(TTTBoardPiece)type
+{
+    return [self checkRowsForWinner] == type || [self checkColsForWinner] == type || [self checkDiagForWinner] == type;
+}
+
+- (TTTBoardPiece)checkRowsForWinner
+{
+    for (int i = 0; i < TTTGameBoardRows; i++) {
+        int xCount = 0;
+        int oCount = 0;
+        
+        for (int j = 0; j < TTTGameBoardCols; j++) {
+            NSInteger currIndex = i * TTTGameBoardRows + j;
+            
+            TTTBoardPiece currPiece = [self.gameBoard.gameMatrix[currIndex] intValue];
+            
+            if (currPiece == TTTBoardPieceX) {
+                xCount++;
+            }
+            
+            if (currPiece == TTTBoardPieceO) {
+                oCount++;
+            }
+            
+            if (xCount == 3) {
+                return TTTBoardPieceX;
+            }
+            
+            if (oCount == 3) {
+                return TTTBoardPieceO;
+            }
+            
+        }
+    }
+    
+    return TTTBoardPieceNone;
+}
+
+- (TTTBoardPiece)checkColsForWinner
+{
+    for (int i = 0; i < TTTGameBoardCols; i++) {
+        int xCount = 0;
+        int oCount = 0;
+        
+        for (int j = 0; j < TTTGameBoardRows; j++) {
+            NSInteger currIndex = i + j * 3;
+            
+            TTTBoardPiece currPiece = [self.gameBoard.gameMatrix[currIndex] intValue];
+            
+            if (currPiece == TTTBoardPieceX) {
+                xCount++;
+            }
+            
+            if (currPiece == TTTBoardPieceO) {
+                oCount++;
+            }
+            
+            if (xCount == 3) {
+                return TTTBoardPieceX;
+            }
+            
+            if (oCount == 3) {
+                return TTTBoardPieceO;
+            }
+            
+        }
+    }
+    
+    return TTTBoardPieceNone;
+}
+
+- (TTTBoardPiece)checkDiagForWinner
+{
+    // Upper left to lower right
+    int xCount = 0;
+    int oCount = 0;
+    
+    for (int i = 0; i < 9; i += 4) {
+        TTTBoardPiece currPiece = [self.gameBoard.gameMatrix[i] intValue];
+        
+        if (currPiece == TTTBoardPieceX) {
+            xCount++;
+        }
+        
+        if (currPiece == TTTBoardPieceO) {
+            oCount++;
+        }
+        
+        if (xCount == 3) {
+            return TTTBoardPieceX;
+        }
+        
+        if (oCount == 3) {
+            return TTTBoardPieceO;
+        }
+    }
+    
+    xCount = 0;
+    oCount = 0;
+    
+    // Upper right to lower left
+    for (int i = 2; i < 7; i += 2) {
+        TTTBoardPiece currPiece = [self.gameBoard.gameMatrix[i] intValue];
+        
+        if (currPiece == TTTBoardPieceX) {
+            xCount++;
+        }
+        
+        if (currPiece == TTTBoardPieceO) {
+            oCount++;
+        }
+        
+        if (xCount == 3) {
+            return TTTBoardPieceX;
+        }
+        
+        if (oCount == 3) {
+            return TTTBoardPieceO;
+        }
+    }
+    
+    return TTTBoardPieceNone;
+}
+
+- (NSInteger)score
+{
+    if ([self hasWonPlayer:TTTBoardPieceX]) {
+        return 10;
+    } else if ([self hasWonPlayer:TTTBoardPieceO]) {
+        return -10;
+    } else {
+        return 0;
+    }
+}
+
 @end
--- TicTacToe/TicTacToe/TTTGameViewController.m
+++ TicTacToe/TicTacToe/TTTGameViewController.m
@@ -8,12 +8,16 @@
 
 #import "TTTGameViewController.h"
 #import "TTTGridView.h"
+#import "TTTCircleView.h"
+#import "TTTXView.h"
 
 @interface TTTGameViewController ()
 
 @property (strong, nonatomic) TTTGridView *gridView;
 @property (strong, nonatomic) UILabel *playerLabel;
+@property (strong, nonatomic) TTTXView *playerIcon;
 @property (strong, nonatomic) UILabel *computerLabel;
+@property (strong, nonatomic) TTTCircleView *computerIcon;
 @property (strong, nonatomic) UILabel *promptLabel;
 
 @end
@@ -37,9 +41,18 @@
     self.gridView.center = CGPointMake(self.view.center.x, self.view.center.y);
     [self.view addSubview:self.gridView];
     
-    self.computerLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.view.bounds.origin.x, self.view.bounds.origin.y + self.view.bounds.size.height - 100, 100, 100)];
+    self.promptLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 200, 100)];
+    self.promptLabel.text = @"Select who goes first:";
+    self.promptLabel.textAlignment = NSTextAlignmentCenter;
+    self.promptLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:20];
+    self.promptLabel.center = CGPointMake(self.view.center.x, self.gridView.center.y + self.gridView.bounds.size.height / 2 + 30);
+    [self.view addSubview:self.promptLabel];
+    
+    self.computerIcon = [[TTTCircleView alloc] initWithFrame:CGRectMake(self.view.bounds.origin.x + 20, self.promptLabel.center.y + 20, 30, 30)];
+    [self.view addSubview:self.computerIcon];
+    
+    self.computerLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.view.bounds.origin.x + 20, self.computerIcon.center.y, 100, 70)];
     self.computerLabel.text = @"You";
-    self.computerLabel.textAlignment = NSTextAlignmentCenter;
     self.computerLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:20];
     [self.computerLabel setUserInteractionEnabled:YES];
     [self.view addSubview:self.computerLabel];
@@ -47,32 +60,31 @@
     UITapGestureRecognizer *computerRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(selectComputer:)];
     [self.computerLabel addGestureRecognizer:computerRecognizer];
     
-    self.playerLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.view.bounds.origin.x + self.view.bounds.size.width - 90, self.view.bounds.origin.y + self.view.bounds.size.height - 100, 100, 100)];
+    self.playerIcon = [[TTTXView alloc] initWithFrame:CGRectMake(self.view.bounds.origin.x + self.view.bounds.size.width - 50, self.promptLabel.center.y + 20, 30, 30)];
+    [self.view addSubview:self.playerIcon];
+    
+    self.playerLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.view.bounds.origin.x + self.view.bounds.size.width - 120, self.playerIcon.center.y, 100, 70)];
     self.playerLabel.text = @"Me";
-    self.playerLabel.textAlignment = NSTextAlignmentCenter;
     self.playerLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:20];
+    self.playerLabel.textAlignment = NSTextAlignmentRight;
     [self.playerLabel setUserInteractionEnabled:YES];
     [self.view addSubview:self.playerLabel];
     
     UITapGestureRecognizer *playerRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(selectPlayer:)];
     [self.playerLabel addGestureRecognizer:playerRecognizer];
     
-    self.promptLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 200, 100)];
-    self.promptLabel.text = @"Select who goes first:";
-    self.promptLabel.textAlignment = NSTextAlignmentCenter;
-    self.promptLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:20];
-    self.promptLabel.center = CGPointMake(self.view.center.x, self.view.center.y + 220);
-    [self.view addSubview:self.promptLabel];
 }
 
 - (void)selectComputer:(UITapGestureRecognizer *)recognizer
 {
     NSLog(@"Selected computer");
+    self.computerLabel.textColor = [UIColor blueColor];
 }
 
 - (void)selectPlayer:(UITapGestureRecognizer *)recgonizer
 {
     NSLog(@"Selected player");
+    self.playerLabel.textColor = [UIColor blueColor];
 }
 
 
